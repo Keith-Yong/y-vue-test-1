144bcc94ae503d9381cf0f75931dccfc06805911
keith-Yong <2957831617@qq.com>
2023-02-23 20:08:33 +0800

    使用模板变量props动态绑定图标

--- src/button.vue
+++ src/button.vue
@@ -1,11 +1,15 @@
 <template>
     <button class ="g-button">
+    <!--:等价于v-bind动态绑定，{icon}是模板变量，``是反引号使模板变量生效  -->
+        <svg class="icon"><use :xlink:href="`#I-${icon}`"></use></svg>
         <slot></slot>
     </button>
 </template>
 
 <script>
-   export default {}
+   export default {
+    props:['icon']
+   }
 
 
 </script>
