f983765c192e0d7841b4b10ad79e533eae7f0f5e
keith-Yong <2957831617@qq.com>
2023-02-23 18:06:39 +0800

    将button改为单文件组件

--- button.js
+++
@@ -1,7 +0,0 @@
-
-// 如果在一个组件中声明了字符串，那么这个字符串可以当作标签使用
-Vue.component('g-button',{
-    template: `
-    <button class ="g-button">按钮</button>
-    `
-})
---
+++ src/button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class ="g-button">按钮</button>
+</template>
+
+<script>
+   export default {}
+
+
+</script>
+
+<style lang="scss">
+ .g-button {
+            font-size: var(--font-size);
+            height: var(--button-height);
+            padding: 0 1em;
+            border-radius: var(--border-radius);
+            border: 1px solid var(--border-color);
+            background: var(--button-bg);
+
+             /* 点击按钮后的样式 */
+             &:hover {
+            border-color: var(--border-color-hover);
+        }
+        /* 移除按钮后的样式 */
+            &:active {
+            background-color: var(--button-active-bg);
+        }
+        /*  移除按钮后的样式 */
+            &:focus {
+            outline: none;
+        }
+        }
+       
+
+</style>
