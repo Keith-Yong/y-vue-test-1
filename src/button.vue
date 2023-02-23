710dc6cc0aaff5f34eee4192e9395fb1bac55012
keith-Yong <2957831617@qq.com>
2023-02-23 21:24:45 +0800

    flex布局，定义模板变量控制图标的位置，处理按钮不对齐的问题

--- src/button.vue
+++ src/button.vue
@@ -1,16 +1,19 @@
 <template>
     
-    <button  class ="g-button">
+    <button  class ="g-button" :class="{[`icon-${iconPosition}`]:true}">
     <!--:等价于v-bind动态绑定，{icon}是模板变量，``是反引号使模板变量生效  -->
     <!--  icon变量存在才用svg -->
         <svg v-if="icon" class="icon"><use :xlink:href="`#I-${icon}`"></use></svg>
+        <!-- slot标签内不能添加样式，所以需要加一个div标签 -->
+        <div class="content">
         <slot></slot>
+        </div>
     </button>
 </template>
 
 <script>
    export default {
-    props:['icon']
+    props:['icon','iconPosition']
    }
 
 
@@ -25,6 +28,12 @@
             border: 1px solid var(--border-color);
             background: var(--button-bg);
 
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            // 遇到scss样式中不对齐的统一使用vertical-align
+            vertical-align: middle;
+
              /* 点击按钮后的样式 */
              &:hover {
             border-color: var(--border-color-hover);
@@ -37,7 +46,23 @@
             &:focus {
             outline: none;
         }
-        }
-       
+            > .icon{
+                order: 1;
+            }
+            > .content{
+                order:2;
+            }
+
+            &.icon-right{
+                > .icon{
+                    order: 2;
+                }
+                > .content{
+                    order: 1;
+                }
+
+            }
+            }
+            
 
 </style>
